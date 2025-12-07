f514149c86b3b3f82ab45adccb4813eb0ce18fa1
dasterer <maksimkocubajlo@gmail.com>
2025-12-07 18:32:11 +0100

    feat: Implement Insertion Sort algorithm

--- sorting_algthms.py
+++ sorting_algthms.py
@@ -10,4 +10,14 @@ def bubble_sort(arr):
             break
     return arr
 
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1,n):
+        j = i - 1
+        key = arr[i]
+        while j>=0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+    return arr
 
