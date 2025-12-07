5693ece24e500e7d8d373bd628fc0e2dd1624e13
dasterer <maksimkocubajlo@gmail.com>
2025-12-07 21:33:28 +0100

    feat: Implement Merge Sort algorithm

--- sorting_algthms.py
+++ sorting_algthms.py
@@ -21,3 +21,31 @@ def insertion_sort(arr):
         arr[j+1] = key
     return arr
 
+def merge(left, right):
+    sorted_array = []
+    i=j=0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i+=1
+        else:
+            sorted_array.append(right[j])
+            j+=1
+    
+    sorted_array.extend(left[i:])
+    sorted_array.extend(left[j:])
+    return sorted_array
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr)//2
+
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
+
+
