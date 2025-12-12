a3bb4b53954b43b9cd9cec2a4d149cfb4c85d98d
dasterer <maksimkocubajlo@gmail.com>
2025-12-12 14:29:34 +0100

    fead: Created timer function to measure execution time of algorithm

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
+import numpy as np
 import random
 import algorithms as alg
+import statistics as st
 import time
 
-def generate_list(size: int,) -> list:
-    return [random.randint(1, 100) for _ in range(size)]
+def generate_list(size: int) -> list:
+    return [random.randint(0, 100) for _ in range(size)]
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        return result, end-start
+    return wrapper
+
+
+    
