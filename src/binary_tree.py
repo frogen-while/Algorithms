538a5695c461ea56b730c6adb428c34492abc362
dasterer <141071038+frogen-while@users.noreply.github.com>
2026-01-10 20:30:06 +0100

    refactor: Update BinaryTree, BinarySearchTree and TernarySearchTree methods to use static methods for traversal and search operations

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -25,17 +25,18 @@ class BinaryTree:
     def __str__(self):
             if self.root is None:
                 return "Empty Tree"
-            return self._build_tree_string(self.root, 0, "Root: ")
+            return BinaryTree._build_tree_string(self.root, 0, "Root: ")
 
-    def _build_tree_string(self, node, level, prefix):
+    @staticmethod
+    def _build_tree_string(node, level, prefix):
         res = " " * (level * 4) + prefix + str(node.key) + "\n"
         if node.left or node.right:
             if node.left:
-                res += self._build_tree_string(node.left, level + 1, "L--- ")
+                res += BinaryTree._build_tree_string(node.left, level + 1, "L--- ")
             else:
                 res += " " * ((level + 1) * 4) + "L--- None\n"
             if node.right:
-                res += self._build_tree_string(node.right, level + 1, "R--- ")
+                res += BinaryTree._build_tree_string(node.right, level + 1, "R--- ")
             else:
                 res += " " * ((level + 1) * 4) + "R--- None\n"
         return res
@@ -64,37 +65,40 @@ class BinaryTree:
 
     def in_order(self):
         result = []
-        self._in_order_recursive(self.root, result)
+        BinaryTree._in_order_recursive(self.root, result)
         return result 
     
-    def _in_order_recursive(self, root, result):
+    @staticmethod
+    def _in_order_recursive(root, result):
         if root != None:
-            self._in_order_recursive(root.left, result)
+            BinaryTree._in_order_recursive(root.left, result)
             result.append(root.key)
-            self._in_order_recursive(root.right, result)
+            BinaryTree._in_order_recursive(root.right, result)
             return result 
     
     def pre_order(self):
         result = []
-        self._pre_order_recursive(self.root, result)
+        BinaryTree._pre_order_recursive(self.root, result)
         return result
     
-    def _pre_order_recursive(self, root, result):
+    @staticmethod
+    def _pre_order_recursive(root, result):
         if root != None:
             result.append(root.key)
-            self._pre_order_recursive(root.left, result)
-            self._pre_order_recursive(root.right, result)
+            BinaryTree._pre_order_recursive(root.left, result)
+            BinaryTree._pre_order_recursive(root.right, result)
             return result 
     
     def post_order(self):
         result = []
-        self._post_order_recursive(self.root, result)
+        BinaryTree._post_order_recursive(self.root, result)
         return result
     
-    def _post_order_recursive(self, root, result):
+    @staticmethod
+    def _post_order_recursive(root, result):
         if root != None:
-            self._post_order_recursive(root.left, result)
-            self._post_order_recursive(root.right, result)
+            BinaryTree._post_order_recursive(root.left, result)
+            BinaryTree._post_order_recursive(root.right, result)
             result.append(root.key)
             return result 
     
@@ -104,23 +108,25 @@ class BinaryTree:
         return 1 + max(self.get_height(node.left), self.get_height(node.right))
     
     def search(self, target):
-        return self._search_recursive( self.root, target)
-        
-    def _search_recursive(self, node, target):
+        return BinaryTree._search_recursive(self.root, target)
+    
+    @staticmethod
+    def _search_recursive(node, target):
         if node is None:
             return False
         if node.key == target:
             return True
         
-        res1 = self._search_recursive(node.left, target)
+        res1 = BinaryTree._search_recursive(node.left, target)
         if res1:
             return True
         
-        res2 = self._search_recursive(node.right, target)
+        res2 = BinaryTree._search_recursive(node.right, target)
         if res2:
             return True  
               
-    def _delete_deepest(self, root, dnode):
+    @staticmethod
+    def _delete_deepest(root, dnode):
         queue = [root]
 
         while queue:
@@ -174,7 +180,7 @@ class BinaryTree:
         if keyNode is not None:
             x = curr.key
             keyNode.key = x
-            self._delete_deepest(self.root, curr)
+            BinaryTree._delete_deepest(self.root, curr)
 
         return self.root
     
@@ -183,21 +189,6 @@ class BinaryTree:
         for key in order:
             self.delete(key)
 
-    def display(self, node=None, level=0, prefix="Root: "):
-        if node is None:
-            node = self.root
-        if node is not None:
-            print(" " * (level * 4) + prefix + str(node.key))
-            if node.left or node.right:
-                if node.left:
-                    self.display(node.left, level + 1, "L--- ")
-                else:
-                    print(" " * ((level + 1) * 4) + "L--- None")
-                if node.right:
-                    self.display(node.right, level + 1, "R--- ")
-                else:
-                    print(" " * ((level + 1) * 4) + "R--- None")
-
 class BinarySearchTree(BinaryTree):
     def __init__(self):
         super().__init__()
@@ -205,43 +196,46 @@ class BinarySearchTree(BinaryTree):
         return super().__str__()
         
     def insert(self, key):
-        self.root = self._insert_recursive(self.root, key)
+        self.root = BinarySearchTree._insert_recursive(self.root, key)
 
-    def _insert_recursive(self, cur_node, key):
+    @staticmethod
+    def _insert_recursive(cur_node, key):
         if cur_node is None:
             return Node(key)
         else:
             if key < cur_node.key:
-                cur_node.left = self._insert_recursive(cur_node.left, key)
+                cur_node.left = BinarySearchTree._insert_recursive(cur_node.left, key)
             else:
-                cur_node.right = self._insert_recursive(cur_node.right, key)
+                cur_node.right = BinarySearchTree._insert_recursive(cur_node.right, key)
         return cur_node
     
     
     def search(self, target):
-        return self._search_recursive(target, self.root)
+        return BinarySearchTree._search_recursive(target, self.root)
 
-    def _search_recursive(self, target, node):
+    @staticmethod
+    def _search_recursive(target, node):
         if node is None:
             return False
         if node.key == target:
             return True
         if target > node.key:
-            return self._search_recursive(target, node.right)
+            return BinarySearchTree._search_recursive(target, node.right)
         else:
-            return self._search_recursive(target, node.left)
+            return BinarySearchTree._search_recursive(target, node.left)
     
         
     def delete(self, target):
-        self.root = self._delete_recursion(self.root, target)
+        self.root = BinarySearchTree._delete_recursion(self.root, target)
 
-    def _delete_recursion(self, root, target):
+    @staticmethod
+    def _delete_recursion(root, target):
         if root is None:
             return root
         if target < root.key:
-            root.left = self._delete_recursion(root.left, target)
+            root.left = BinarySearchTree._delete_recursion(root.left, target)
         elif target > root.key:
-            root.right = self._delete_recursion(root.right, target)
+            root.right = BinarySearchTree._delete_recursion(root.right, target)
 
         else:
 
@@ -250,19 +244,21 @@ class BinarySearchTree(BinaryTree):
             elif root.right is None:
                 return root.left
 
-            temp = self._min_value_node(root.right)
+            temp = BinarySearchTree._min_value_node(root.right)
             root.key = temp.key
-            root.right = self._delete_recursion(root.right, temp.key)
+            root.right = BinarySearchTree._delete_recursion(root.right, temp.key)
 
         return root
 
-    def _min_value_node(self, node):
+    @staticmethod
+    def _min_value_node(node):
         current = node
         while current.left is not None:
             current = current.left
         return current
     
-    def _delete_deepest(self, root, dnode):
+    @staticmethod
+    def _delete_deepest(root, dnode):
         raise NotImplementedError("BinarySearchTree deletion uses a different logic and doesn't need _delete_deepest")
     
 
@@ -270,7 +266,8 @@ class TernarySearchTree:
     def __init__(self):
         self.root = None
 
-    def _is_full(self, node):
+    @staticmethod
+    def _is_full(node):
         return len(node.key) == 2
 
 
@@ -278,15 +275,15 @@ class TernarySearchTree:
         if self.root is None:
             self.root = TernaryNode(value)
         else:
-            self._insert_recursive(self.root, value)
+            TernarySearchTree._insert_recursive(self.root, value)
         
         
-
-    def _insert_recursive(self, cur_node, value):
+    @staticmethod
+    def _insert_recursive(cur_node, value):
         if value in cur_node.key:
             return
 
-        if not self._is_full(cur_node):
+        if not TernarySearchTree._is_full(cur_node):
             cur_node.key.append(value)
             cur_node.key.sort()
             return 
@@ -295,42 +292,43 @@ class TernarySearchTree:
             if cur_node.left is None:
                 cur_node.left = TernaryNode(value)
             else:
-                self._insert_recursive(cur_node.left, value)
+                TernarySearchTree._insert_recursive(cur_node.left, value)
 
         elif value > cur_node.key[1]:
             if cur_node.right is None:
                 cur_node.right = TernaryNode(value)
             else:
-                self._insert_recursive(cur_node.right, value)
+                TernarySearchTree._insert_recursive(cur_node.right, value)
 
         else:
             if cur_node.middle is None:
                 cur_node.middle = TernaryNode(value)
             else:
-                self._insert_recursive(cur_node.middle, value)
+                TernarySearchTree._insert_recursive(cur_node.middle, value)
     
     
     def delete(self, value):
-        self.root = self._delete_recursive(self.root, value)
+        self.root = TernarySearchTree._delete_recursive(self.root, value)
 
-    def _delete_recursive(self, node, value):
+    @staticmethod
+    def _delete_recursive(node, value):
         if node is None:
             return None
 
         if value < node.key[0]:
-            node.left = self._delete_recursive(node.left, value)
+            node.left = TernarySearchTree._delete_recursive(node.left, value)
             return node
         
         if len(node.key) == 2 and value > node.key[1]:
-            node.right = self._delete_recursive(node.right, value)
+            node.right = TernarySearchTree._delete_recursive(node.right, value)
             return node
             
         if len(node.key) == 2 and node.key[0] < value < node.key[1]:
-            node.middle = self._delete_recursive(node.middle, value)
+            node.middle = TernarySearchTree._delete_recursive(node.middle, value)
             return node
         
         if len(node.key) == 1 and value > node.key[0]:
-             node.middle = self._delete_recursive(node.middle, value)
+             node.middle = TernarySearchTree._delete_recursive(node.middle, value)
              return node
 
         if node.left is None and node.middle is None and node.right is None:
@@ -344,9 +342,9 @@ class TernarySearchTree:
         if value == node.key[0]:
 
             if node.middle is not None:
-                successor_val = self._get_min_value(node.middle)
+                successor_val = TernarySearchTree._get_min_value(node.middle)
                 node.key[0] = successor_val 
-                node.middle = self._delete_recursive(node.middle, successor_val)
+                node.middle = TernarySearchTree._delete_recursive(node.middle, successor_val)
             else:
                 node = node.left 
         
@@ -354,10 +352,10 @@ class TernarySearchTree:
         elif len(node.key) == 2 and value == node.key[1]:
 
             if node.right is not None:
-                successor_val = self._get_min_value(node.right)
+                successor_val = TernarySearchTree._get_min_value(node.right)
                 node.key[1] = successor_val 
 
-                node.right = self._delete_recursive(node.right, successor_val)
+                node.right = TernarySearchTree._delete_recursive(node.right, successor_val)
             else:
 
                  node.key.pop()
@@ -365,17 +363,19 @@ class TernarySearchTree:
             return None
             
         return node
-        
-    def _get_min_value(self, node):
+    
+    @staticmethod
+    def _get_min_value(node):
         current = node
         while current.left is not None:
             current = current.left
         return current.key[0]
     
     def search(self, target):
-        return self._search_recursive(target, self.root)
+        return TernarySearchTree._search_recursive(target, self.root)
 
-    def _search_recursive(self, target, node):
+    @staticmethod
+    def _search_recursive(target, node):
         if node is None:
             return False
 
@@ -383,64 +383,63 @@ class TernarySearchTree:
             return True
         
         if target < node.key[0]:
-            return self._search_recursive(target, node.left)
+            return TernarySearchTree._search_recursive(target, node.left)
         
         if len(node.key) == 2 and target > node.key[1]:
-            return self._search_recursive(target, node.right)
+            return TernarySearchTree._search_recursive(target, node.right)
     
-        return self._search_recursive(target, node.middle)
+        return TernarySearchTree._search_recursive(target, node.middle)
 
     def in_order(self):
-        result = []
-        self._in_order_recursive(self.root, result)
+        result = [] 
+        TernarySearchTree._in_order_recursive(self.root, result)
         return result
-
-    def _in_order_recursive(self, root, result):
+    
+    @staticmethod
+    def _in_order_recursive(root, result):
         if root is None:
             return 
-
-        self._in_order_recursive(root.left, result)
+        
+        TernarySearchTree._in_order_recursive(root.left, result)
         result.append(root.key[0])
-        self._in_order_recursive(root.middle, result)
+        TernarySearchTree._in_order_recursive(root.middle, result)
 
         if len(root.key) == 2:
             result.append(root.key[1])
-            self._in_order_recursive(root.right, result)
-        return result
+            TernarySearchTree._in_order_recursive(root.right, result)
     
     def pre_order(self):
-        result = []
-        self._pre_order_recursive(self.root, result)
+        result = [] 
+        TernarySearchTree._pre_order_recursive(self.root, result)
         return result
 
-    def _pre_order_recursive(self, node, result):
+    @staticmethod
+    def _pre_order_recursive(node, result):
         if node is None:
             return 
         
         result.extend(node.key)
-        self._pre_order_recursive(node.left, result)
-        self._pre_order_recursive(node.middle, result)
+        TernarySearchTree._pre_order_recursive(node.left, result)
+        TernarySearchTree._pre_order_recursive(node.middle, result)
 
         if len(node.key) == 2:
-            self._pre_order_recursive(node.right, result)
-
-        return result
-        
+            TernarySearchTree._pre_order_recursive(node.right, result)
     
     def post_order(self):
-        result = []
-        self._post_order_recursive(self.root, result)
+        result = [] 
+        TernarySearchTree._post_order_recursive(self.root, result)
         return result
 
-    
-    def _post_order_recursive(self, node, result):
+    @staticmethod
+    def _post_order_recursive(node, result):
         if node is None:
             return
-        self._post_order_recursive(node.left, result)
-        self._post_order_recursive(node.middle, result)
+        
+        TernarySearchTree._post_order_recursive(node.left, result)
+        TernarySearchTree._post_order_recursive(node.middle, result)
 
         if len(node.key) == 2:
-            self._post_order_recursive(node.right, result)
+            TernarySearchTree._post_order_recursive(node.right, result)
 
         result.extend(node.key)
 
